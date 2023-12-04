87515007e17ede50d184e8666180bd99d8414316
Davide Bertalero <davide.bertalero@studenti.unimi.it>
2023-12-04 17:53:00 +0100

    creato nodlet per filtrare scan

---
+++ src/tirocinio/scripts/lidar_maxrange_filter.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+import rospy
+from sensor_msgs.msg import LaserScan
+import math
+
+class lidar_maxrange_filter:
+
+    def __init__(self):
+        #rospy.loginfo("inizializzo")
+        self.pub = rospy.Publisher('scan_maxrange_filtered', LaserScan, queue_size=10)
+        self.sub = rospy.Subscriber("scan", LaserScan, self.callback)
+
+    def callback(self, scan):
+        #rospy.loginfo("CALLBACK")
+        filtered = scan 
+        ranges = list(filtered.ranges)
+        #for i in range(len(ranges)):
+        #    if math.isnan(ranges[i]):
+        #        #rospy.loginfo("corretto valore")
+        #        ranges[i] = 4.0
+        #filtered.ranges = tuple(ranges)
+        filtered.range_max = 4.0
+        self.pub.publish(filtered)
+
+    
+def main():
+
+    lf = lidar_maxrange_filter()
+    rospy.init_node('lidar_maxrange_filter', anonymous=True)
+    #rospy.loginfo("sono nel main")
+
+    # spin() simply keeps python from exiting until this node is stopped
+    rospy.spin()
+    
+
+if __name__ == '__main__':
+    main()
