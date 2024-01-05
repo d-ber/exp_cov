26d0d01af479636f6d9a0f1f27873b6571776988
davide.bertalero <davide.bertalero@studenti.unimi.it>
2024-01-05 15:15:22 +0100

    aggiunta lettura rettangoli da json

--- src/tirocinio/scripts/pose_check.py
+++ src/tirocinio/scripts/pose_check.py
@@ -1,12 +1,38 @@
 #!/usr/bin/env python
 import rospy
+import sys
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Point
+import os
+import json
 
 class Rectangle:
-    def __init__(self, min_point, max_point):
-        self.min_point = min_point
-        self.max_point = max_point
+
+    def __init__(self, min_point=None, max_point=None, center_x=None, center_y=None, width=None, height=None):
+        if min_point is not None and max_point is not None:
+            self.min_point = min_point
+            self.max_point = max_point
+            self.calculate_center_dimensions()
+        elif center_x is not None and center_y is not None and width is not None and height is not None:
+            self.center_x = center_x
+            self.center_y = center_y
+            self.width = width
+            self.height = height
+            self.calculate_min_max_points()
+        else:
+            raise ValueError("Invalid parameters. Please provide either min_point and max_point or center_x, center_y, width, and height.")
+
+    def calculate_min_max_points(self):
+        half_width = self.width / 2
+        half_height = self.height / 2
+        self.min_point = (self.center_x - half_width, self.center_y - half_height)
+        self.max_point = (self.center_x + half_width, self.center_y + half_height)
+
+    def calculate_center_dimensions(self):
+        self.center_x = (self.min_point[0] + self.max_point[0]) / 2
+        self.center_y = (self.min_point[1] + self.max_point[1]) / 2
+        self.width = abs(self.max_point[0] - self.min_point[0])
+        self.height = abs(self.max_point[1] - self.min_point[1])
 
 class pose_check:
 
@@ -26,22 +52,45 @@ class pose_check:
                 return
         rospy.loginfo("Position is outside all rectangles: x={}, y={}".format(position.x, position.y))
 
+def read_rectangles(json_file_path):
+    rectangles = []
+
+    # Read JSON data from file
+    try:
+        with open(json_file_path, 'r') as json_file:
+            json_data = json.load(json_file)
+    except json.JSONDecodeError as e:
+        print(f"Error decoding JSON at '{json_file_path}': {e}")
+        return rectangles
+
+    # Create Rectangle objects from JSON data
+    for rect in json_data:
+        rectangle = Rectangle(
+            center_x=info["center"][0],
+            center_y=info["center"][1],
+            width=info["width"],
+            height=info["height"]
+        )
+        rectangles.append(rectangle)
+
+    return rectangles
+
 
 def main():
 
-    # Define multiple rectangles
-    rectangles = [
-        Rectangle(min_point=Point(x=-6.0, y=0.0, z=0.0), max_point=Point(x=0.0, y=7.0, z=0.0))
-    ]
-
-    check_frequency = 10.0
-
+    # Read rectangles from json file
+    json_file_path = os.path.join(os.getcwd(), "rectangles.json")
+    rectangles = read_rectangles(json_file_path)
+    if len(rectangles) == 0:
+        print("No Rectangles Given!")
+        
     rospy.init_node('pose_check', anonymous=True)
-    lf = pose_check(rectangles)
+    _ = pose_check(rectangles)
 
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
-    
+
+
 
 if __name__ == '__main__':
     main()
