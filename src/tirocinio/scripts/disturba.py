4ba6eea13016e496b3dc5c4f6aaf9f6516e726b2
Davide Bertalero <davide.bertalero@studenti.unimi.it>
2024-01-10 14:31:59 +0100

    migliorata gestione argomenti da cmd

--- src/tirocinio/scripts/disturba.py
+++ src/tirocinio/scripts/disturba.py
@@ -1,6 +1,7 @@
 import pyautogui
 import time
 import sys
+import argparse
 import scipy.stats as st
 
 # Function to center the mouse in a specified region
@@ -40,29 +41,31 @@ def disturb(movement, rotation):
     drag(movement, 1, "left") # To move the robot
     drag(rotation, 1, "right") # To rotate the robot
 
+def parse_args():
+    parser = argparse.ArgumentParser(description='"Disturb" a robot in stage simulator.')
+    parser.add_argument('--map', default=1, choices=[1, 2, 3], type=int,
+        help="Number of rgb map.")
+    parser.add_argument('--mode', default="full", choices=["full", "disturb", "select"],
+        help="Mode in which to launch the program. Select only selects the robot and zooms in. Disturb only distrubs it. Full does both.")
+    return parser.parse_args()
+
 def main():
 
-    # Possible and default mode, map_rgb values
-    modes = ("select", "disturb", "full")
-    maps_rgb = ("1", "2", "3")
-    mode = "full"
-    map_rgb = 1
-
-    if len(sys.argv) > 2 and sys.argv[1] in modes and sys.argv[2] in maps_rgb:
-        mode = sys.argv[1]
-        map_rgb = int(sys.argv[2])
+    args = parse_args()
+    mode = args.mode
+    map_rgb = args.map
 
     # Window coordinates and size
     #Lab:
     #window_x = 574
     #window_y = 196
-    #Home:
+    #Home:f
     window_x = 904
     window_y = 421
     window_width = 700
     window_height = 660
 
-    # Click locations (xs[0] is for map_rgb number 1, etc...)
+    # Click locations (xs[0] is for map_rgb number 1, etc...)f
     #Lab:
     #xs = (785, 0, 0)
     #ys = (430, 0, 0)
