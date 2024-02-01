d0ce47ea1dcb295a832f4946cc6a2d23312d5cd0
davide.bertalero <davide.bertalero@studenti.unimi.it>
2024-02-01 15:42:55 +0100

    aggiornato modify_and_run

--- src/tirocinio/scripts/modify_and_run.py
+++ src/tirocinio/scripts/modify_and_run.py
@@ -1,48 +1,78 @@
 import subprocess as sp
 import os
+import progressbar
 import argparse
-import rospkg
+
+def make_map(args, world_num):
+    image_path = args.map
+    movement_mask_image_path = args.mask
+    speedup = args.speedup
+    pose = args.pose
+    scale = args.scale
+
+    sp.run(["python3", "/home/d-ber/catkin_ws/src/tirocinio/scripts/map_rgb_simul.py", "--map", image_path, "--mask", movement_mask_image_path,
+        "--dir", os.path.join(os.getcwd(), "worlds"), "--speedup", str(speedup), "--pose", f"{pose[0]} {pose[1]}", "--scale", str(scale), "--world-num", str(world_num)])
+
+    return(f"world{world_num}.world")
+
+def main(args):
+    mapName = make_map(args, 0)
+    mapName = os.path.join(os.getcwd(), "worlds", str(mapName))
+    rect_path = os.path.join(os.path.dirname(mapName), f"bitmaps/rectangles0.json")
+    world_path = os.path.join(os.getcwd(), "worlds", str(mapName))
+    
+    print(["roslaunch", "tirocinio", "stage_init_select.launch", "worldfile:=" + world_path, "rectangles:=" + rect_path])
+    sp.run(["roslaunch", "tirocinio", "stage_init_select.launch", "worldfile:=" + world_path, "rectangles:=" + rect_path])
+    
+
+
+def check_positive(value):
+    try:
+        value = int(value)
+        if value <= 0:
+            raise argparse.ArgumentTypeError("{} is not a positive integer".format(value))
+    except ValueError:
+        raise Exception("{} is not an integer".format(value))
+    return value
+
+def check_pose(value):
+    try:
+        ret_value = value.split()
+        if len(ret_value) != 2:
+            raise argparse.ArgumentTypeError(f"Given pose value \"{value}\" is not made of 2 numbers")
+        return (float(ret_value[0]), float(ret_value[1]))
+    except ValueError:
+        raise Exception("{} is not made of 2 numbers".format(value))
+
+def check_positive_float(value):
+    try:
+        value = float(value)
+        if value <= 0:
+            raise argparse.ArgumentTypeError("{} is not a positive float".format(value))
+    except ValueError:
+        raise Exception("{} is not a float".format(value))
+    return value
 
 def parse_args():
-    parser = argparse.ArgumentParser(description='Modify a map and run stage on it.')
-    parser.add_argument('--map', default=1, choices=[1, 2, 3, 4], type=int,
-        help="Number of rgb map.")
-    parser.add_argument('--scan', default="/scan", help="Scan topic name to set for stage.")
-    parser.add_argument('--screen', default="home", choices=["home", "lab"],
-        help="Screen for the disturb node.")
-    return parser.parse_known_args()[0]
+    parser = argparse.ArgumentParser(description='Start exploration in docker containers.')
+    parser.add_argument('--map', default=os.path.join(os.getcwd(), "src/tirocinio/maps_rgb_lab/map1/map1_rgb.png"),
+        help="Path to the rgb map file.", metavar="MAP_PATH")
+    parser.add_argument('--mask', default=os.path.join(os.getcwd(), "src/tirocinio/maps_rgb_lab/map1/map1_movement_mask.png"),
+        help="Path to the rgb mask map file for movement areas. Each rgb object in the map will be moved within the yellow mask given in this file. If the object is none, then it can move freely.", metavar="MASK_PATH")
+    parser.add_argument("--speedup", type=check_positive, default=10, metavar="SPEEDUP",
+        help="Use this to adjust stage simulation speed. Higher is faster but heavier on the CPU.") 
+    parser.add_argument('--pose', type=check_pose, default=(0, 0), metavar="X Y",
+        help="Robot pose X and Y coordinates.")
+    parser.add_argument('--scale', type=check_positive_float, default=0.035888, metavar="PIXELS",
+        help="Number of meters per pixel in png map.")
+    return parser.parse_args()
 
-def main():
-    # get an instance of RosPack with the default search paths
-    rospack = rospkg.RosPack()
-    # get the file path for tirocinio
-    package_path = rospack.get_path('tirocinio')
+if __name__ == "__main__":
 
     args = parse_args()
-    map_rgb = str(args.map)
-    map_rgb_path = os.path.join(package_path, "maps_rgb_lab/map" + map_rgb + "/map" + map_rgb + "_rgb.png")
-    movement_mask_path = os.path.join(package_path, "maps_rgb_lab/map" + map_rgb + "/map" + map_rgb + "_movement_mask.png")
-    scan_topic = args.scan
-    screen = args.screen
 
-    image_path = os.path.join(package_path, "world/bitmaps", "image.png")
-    rects_path = os.path.join(package_path, "world/bitmaps", "rectangles.json")
-    world_path = os.path.join(package_path, "world/rgb.world")
+    main(args)
+
+    print("All workers finished")
 
 
-    if os.path.exists(image_path):
-        os.remove(image_path)
-    if os.path.exists(rects_path):
-        os.remove(rects_path)
-
-    try:
-        sp.run(["rosrun", "tirocinio", "map_rgb_simul.py", "--save", "--no-timestamp", "--map", map_rgb_path, "--mask", movement_mask_path,
-            "--dir", os.path.join(package_path, "world/bitmaps")])
-
-        sp.run(["roslaunch", "tirocinio", "stage_init_select.launch", "rectangles:=" + rects_path, 
-        "map_rgb:=1", "worldfile:=" + world_path, "scan:=" + scan_topic, "screen:=" + screen])
-    except KeyboardInterrupt:
-        print("Exit request received. Closed.")
-
-if __name__ == '__main__':
-    main()
