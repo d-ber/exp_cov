334ed5890c10ca61d5777c35f9873c2a748c2578
davide.bertalero <davide.bertalero@studenti.unimi.it>
2024-02-17 18:01:21 +0100

    aggiunto supporto buchi

--- src/tirocinio/scripts/optimization_data.py
+++ src/tirocinio/scripts/optimization_data.py
@@ -49,9 +49,13 @@ def distanza_gg(distanze_gg_data):
         dis_gg.insert(i2, math.dist(g1,g2))
     return (i, dis_gg)
 
+def min_distance_to_holes(poly_with_holes, point):
+    return min([hole.distance(point) for hole in poly_with_holes.interiors])
+
 def print_dat(poly):
     GUARD_RESOLUTION = 3
     WITNESS_RESOLUTION = 5
+    MIN_DISTANCE_TO_POLY = 5
     print("\ndata;")
 
     print_simple_param("coeff_coverage", 1)
@@ -64,7 +68,8 @@ def print_dat(poly):
     # bounds Returns minimum bounding region (minx, miny, maxx, maxy)
     for x in range(round(poly.bounds[0]), round(poly.bounds[2]), GUARD_RESOLUTION):
         for y in range(round(poly.bounds[1]), round(poly.bounds[3]), GUARD_RESOLUTION):
-            if poly.contains(shapely.Point([x, y])) and poly.exterior.distance(shapely.Point([x, y])) >= 5:
+            guard_candidate = shapely.Point([x, y])
+            if poly.contains(guard_candidate) and poly.exterior.distance(guard_candidate) >= MIN_DISTANCE_TO_POLY and min_distance_to_holes(poly, guard_candidate) >= MIN_DISTANCE_TO_POLY:
                 guards.append((x,y))
     nG = len(guards)
     print_simple_param("nW", nW)
@@ -110,19 +115,40 @@ def solve(poly):
 def main():
 
     img_path = "/home/d-ber/catkin_ws/src/tirocinio/scripts/maps_agp/map_grey_to_black.png"
+    MIN_HOLE_AREA = 10
+    DEBUG_HOLES = False
+    DEBUG_CONTOUR = False 
     img = cv2.imread(img_path)
     img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
     contours, _ = cv2.findContours(img, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
     max_contour = max(contours, key = lambda cnt: cv2.contourArea(cnt))
-    img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
-    debug_contour = False 
-    if debug_contour:
+    if DEBUG_CONTOUR:
         debug = np.zeros_like(img)
         cv2.drawContours(debug, [max_contour], -1, (255,255,255), cv2.FILLED)
         _ = plt.subplot(111), plt.imshow(cv2.cvtColor(debug, cv2.COLOR_BGR2RGB)), plt.title('guards')
         plt.show()
-        exit()
-    poly = shapely.Polygon([[p[0][0], p[0][1]] for p in max_contour])
+    img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+    holes = []
+    for c in contours:
+        if cv2.contourArea(c) != cv2.contourArea(max_contour) and cv2.contourArea(c) >= MIN_HOLE_AREA:
+            holes.append(c)
+            if DEBUG_HOLES:
+                debug = np.zeros_like(img)
+                cv2.drawContours(debug, [c], -1, (255,255,255), cv2.FILLED)
+                _ = plt.subplot(111), plt.imshow(cv2.cvtColor(debug, cv2.COLOR_BGR2RGB)), plt.title('hole')
+                plt.show()
+    poly_no_holes = shapely.Polygon([[p[0][0], p[0][1]] for p in max_contour])
+    poly_no_holes = poly_no_holes.buffer(0)
+    if poly_no_holes.geom_type == 'MultiPolygon': # se poly non è un poligono ben definito provo a renderlo tale
+        poly = max(poly_no_holes.geoms, key=lambda a: a.area)  
+    holes_fixed = []
+    for h in holes:
+        hole = shapely.Polygon([[p[0][0], p[0][1]] for p in h])
+        if hole.geom_type == 'MultiPolygon': # se poly non è un poligono ben definito provo a renderlo tale
+            hole = max(hole.geoms, key=lambda a: a.area) 
+        if poly_no_holes.contains(hole):
+            holes_fixed.append(hole.buffer(0).exterior)
+    poly = shapely.Polygon([[p[0][0], p[0][1]] for p in max_contour], holes=holes_fixed)
     poly = poly.buffer(0)
     if poly.geom_type == 'MultiPolygon': # se poly non è un poligono ben definito provo a renderlo tale
         poly = max(poly.geoms, key=lambda a: a.area)  
