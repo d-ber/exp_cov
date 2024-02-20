ae2569ec9c233ea43f4c18ffc5eefb1560d2deab
Davide Bertalero <davide.bertalero@studenti.unimi.it>
2024-02-20 17:10:57 +0100

    aggiunta prima versione

--- src/tirocinio/scripts/tsp.py
+++ src/tirocinio/scripts/tsp.py
@@ -2,12 +2,44 @@ import numpy as np
 from python_tsp.exact import solve_tsp_dynamic_programming
 
 def main():
-    distance_matrix = np.array([
-    [0,  5, 4, 10],
-    [5,  0, 8,  5],
-    [4,  8, 0,  3],
-    [10, 5, 3,  0]
-    ])
+    dat_path = "/home/aislab/catkin_ws/src/tirocinio/optimization/test7/data.dat"
+    chosen_guards_path = "/home/aislab/catkin_ws/src/tirocinio/optimization/test7/guardie_scelte.txt"
+    distanze = []
+    with open(dat_path, "r") as data_file:
+        line = data_file.readline()
+        while not line.strip().startswith("param nG :="):
+            line = data_file.readline()
+        nG = int(line.split(":=")[1].strip().split(";")[0])
+        line = data_file.readline()
+        while not line.strip().startswith("param distanza :"):
+            line = data_file.readline()
+        line = data_file.readline()
+        while line.strip() != ";":
+            pieces = line.split()
+            _, distanze_g = pieces[0], pieces[1:]
+            distanze.append([float(x) for x in distanze_g])
+            line = data_file.readline()
+    guardie_scelte = []
+    with open(chosen_guards_path, "r") as choice_file:
+        line = choice_file.readline()
+        while line.strip() != "scelta_guardie [*] :=":
+            line = choice_file.readline()
+        line = choice_file.readline()
+        while line.strip() != ";":
+            guardie_scelte.append(float(line.strip().split()[0]))
+            line = choice_file.readline()
+    distanze_scelte = []
+    for g1 in range(len(distanze)):
+        if g1 in guardie_scelte:
+            distanze_g1 = []
+            for g2 in range(len(distanze)):
+                if g2 in guardie_scelte:
+                    distanze_g1.append(distanze[g1][g2])
+            distanze_scelte.append(distanze_g1)
+    
+    print(f"Solving TSP with {len(distanze_scelte)} nodes")
+    
+    distance_matrix = np.array(distanze_scelte, dtype=float)
     permutation, distance = solve_tsp_dynamic_programming(distance_matrix)
     print(permutation, distance)
 
